cc5c957b9b3e10eedd3893f4d035691045fe43ba
System Administrator <root@Hanhs-Laptop.local>
2024-07-31 08:35:14 +0700

    Commit lab for lesson 1 React basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,66 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import avatar from "./assets/avatar.jpeg";
 import stranger from "./assets/stranger.png";
+import javeSpring from "./assets/book-image/java-spring.jpg";
+import linux from "./assets/book-image/linux.jpg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function formatName(user) {
-    return user.firstName + " " + user.lastName;
-  }
-
-  const user = {
-    // firstName: "Hanh",
-    // lastName: "Pham",
-    // age: 20,
-    // title: "Cloud Developer",
-    // avatar: avatar,
-    // hobby: "Coding, Playing Sport, Reading Book",
-  };
-
-  function getGreeting(user) {
-    if (Object.keys(user).length != 0) {
-      return <h1>Hello, My name is {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
-
-  return (
+  const bookList = [
+    {
+      id: "1",
+      name: "Java Spring Boot in Action",
+      author: "Craig Walls",
+      price: "9.99$",
+      des: "",
+      review: "5",
+      image: javeSpring,
+    },
+    {
+      id: "2",
+      name: "Linux for Beginners",
+      author: "Dylan Mach",
+      price: "10.99$",
+      des: "A Comprehesnive Step by Step Starting Guide To Learn Linux from Scratch",
+      review: "4.5",
+      image: linux,
+    },
+  ];
+  const navBar = (
     <>
-      <div className="avatar">
-        <img src={user.avatar || stranger} />
-      </div>
-      <div className="infor">{getGreeting(user)}</div>
-      <div style={{ textAlign: "start" }}>
-        <h3 style={{ margin: "5px" }}>
-          Age: <span style={{ fontWeight: "400" }}>{user.age || "___"}</span>
-        </h3>
-
-        <h3 style={{ margin: "5px" }}>
-          Tile: <span style={{ fontWeight: "400" }}>{user.title || "___"}</span>
-        </h3>
-
-        <h3 style={{ margin: "5px" }}>
-          Hobby: <span style={{ fontWeight: "400" }}>{user.hobby || "___"}</span>
-        </h3>
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "#EC7211",
+          height: "auto",
+          display: "flex",
+          alignItems: "center",
+          padding: "10px 50px",
+          color: "white",
+        }}
+      >
+        <div style={{ fontSize: "26px", fontWeight: "700" }}>Book Store</div>
+        <div style={{ paddingLeft: "20px" }}> Home </div>
       </div>
     </>
+  );
+
+  const bookEle = bookList && bookList.map((book) => {
+    return (
+      <div className="book-container">
+        <img className="book-img" src={book.image} />
+        <div style={{ fontSize: "16px", fontWeight: "500" }}>{book.name}</div>
+        <div style={{ fontSize: "14px" }}>{book.author}</div>
+        <button className="btn-primary">Add to cart</button>
+      </div>
+    );
+  });
+
+  return (
+    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      {navBar}
+      <div className="grid-container">{bookEle}</div>
+    </div>
   );
 }
 
